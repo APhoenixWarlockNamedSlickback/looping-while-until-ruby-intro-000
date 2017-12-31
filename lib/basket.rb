e71c04541e82259fea84c15682c7b368df4df890
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-31 17:17:00 +0000

    automatically backed up by learn

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,4 +1,4 @@
 primary_colors = ["Red", "Yellow", "Blue", "Quadriple", "Inconvenience", "Appleberryfartknockers"]
 primary_colors.each do |color|
-  puts "Primary Color #{color} is #{color.length} letters long."
+  puts "Primary Color #{color} is #{color.length+2} letters long."
 end
