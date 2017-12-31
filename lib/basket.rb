805b59e13b8db4f997491b9a1273ee2b8809500c
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-31 15:48:51 +0000

    automatically backed up by learn

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,4 +1,4 @@
-basket = ["apple 1", "apple 2", "apple 3", "apple 4", "apple 5", "apple 6", "apple 7", "apple 8", "apple 9", "apple 10"]
+basket = ["apple 1", "apple 2", "apple 3", "apple 4", "apple 13", "apple 6", "apple 7", "apple 8", "apple 9", "apple 10"]
 
 apples_in_basket = basket.size
 apples_taken_out = 0
