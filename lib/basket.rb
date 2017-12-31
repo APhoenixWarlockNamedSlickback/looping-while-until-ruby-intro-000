4d7cad6154c55de4402cedb6163d8d20e962d141
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-31 16:36:25 +0000

    automatically backed up by learn

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,6 +1,7 @@
-input = ""
-while input != "mommmmm!!"
-  puts "stop hitting youself!"
-  input = gets.chomp
-end
+brothers = ["Tom", "Tim", "Jim"]
 
+count = 0
+while count <= brothers.length-1
+  puts "stop hitting yourself #{brothers[count]}!"
+  count += 1
+end
